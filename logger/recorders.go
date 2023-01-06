abd2981e72284a05b0ca3bb6f6e380e953acec2d
Ismael Romero <76606976+Ismael-Romero@users.noreply.github.com>
2023-01-06 17:42:27 -0600

    change 2.0.1

--- logger/recorders.go
+++ logger/recorders.go
@@ -47,6 +47,7 @@ func (l *Logger) Response200(r *http.Request, m string) {
 		Level: "info",
 		Date:  time.Now().Format("2006-01-02 15:04:05"),
 		Body: detailsRequest{
+			Message:  m,
 			From:     r.RemoteAddr,
 			Protocol: r.Proto,
 			Method:   r.Method,
